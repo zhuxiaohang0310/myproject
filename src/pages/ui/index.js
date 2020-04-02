de8d1dced857e30e1a46e86f9e659b50504293af
zhuxiaohang <895600200@qq.com>
2020-04-02 19:14:27 +0800

    qqqqqqqqqqqqqqqqqq

--- src/pages/ui/index.js
+++ src/pages/ui/index.js
@@ -5,7 +5,7 @@ export default class UI extends Component {
     render() {
         return (
             <div className = 'homepage'>
-                这里是ui 页面
+                这里是ui 页面 我去你妈个逼
             </div>
         )
     }
